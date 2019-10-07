e9262b59cf03643714e62eb5441c4a9e8ebb4e1c
Josua Blejeru <josua.blejeru@gmail.com>
2019-10-07 20:28:10 +0200

    Login Form with 'username', 'password', 'remember_me' checkbox and a submit Field

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    """ Login Form  """
+    # Each field is given a description or label as a first argument.
+    # The validators argument attach validation behaviors to the fields.
+    # The DataRequrired simply check that a form is not submitted empty
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
