23c028c153ebc8f99ff46092f787c6239fdb5b1d
Josua Blejeru <josua.blejeru@gmail.com>
2019-11-02 01:42:18 +0100

    env`s path deleted

--- app/config.py
+++ app/config.py
@@ -1,11 +1,8 @@
 import os
-from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path('./.env')
-load_dotenv(dotenv_path=env_path, verbose=True)
-
+load_dotenv(verbose=True)
 
 MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
 MYSQL_USER = os.environ.get('MYSQL_USER')
